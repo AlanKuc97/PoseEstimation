48720b181c4115f41fa9d0d0461b592b04f5ebfe
Alanas Kučinskij <alanas.kucinskij@stud.mif.vu.lt>
2020-04-18 22:53:37 +0300

    Added new neural network model

--- high-end/index.js
+++ high-end/index.js
@@ -1,4 +1,14 @@
+/*
+	*** NEURAL NETWORK LABEL MEANING ***
+	G - sitting still 
+	Q - stretching neck to right side
+	W - stretching neck to left side
+	E - stretching neck front
+	R - stretching right arm
+	T - stretching left arm
+*/
 let capture;
+let pose;
 let poseNet;
 let fr = 30;
 let videoHeight = 480;
@@ -11,7 +21,20 @@ let constraints = {
 		},
 		audio: false
 	};
-const MIN_PREDICTION_SCORE = 0.3;
+	
+let neuralNet;
+let parameters = {
+    inputs: 34,
+    outputs: 6,
+    task: 'classification',
+    debug: true
+};
+const modelInfo = {
+    model: '../myModel/model.json',
+    metadata: '../myModel/model_meta.json',
+    weights: '../myModel/model.weights.bin',
+};
+
 function setup(){
 	capture = createCapture(constraints);
 	capture.size(videoWidth,videoHeight);
@@ -20,45 +43,89 @@ function setup(){
 		.position((windowWidth - capture.width) / 2, (windowHeight - capture.height - timeSign.clientHeight) / 2);
 	poseNet = ml5.poseNet(capture,modelReady);
 	poseNet.on('pose',getPoses);
+	neuralNet = ml5.neuralNetwork(parameters);
+	neuralNet.load(modelInfo, neuralNetLoaded);
 }
+
 function modelReady(){
-	console.log('Model ready!');
+	console.log('Posenet model ready');
 }
+
+function neuralNetLoaded() {
+  console.log('Classification model ready');
+  classifyPose();
+}
+
+function classifyPose() {
+  if(pose){
+    let inputs = [];
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      inputs.push(x);
+      inputs.push(y);
+    }
+    neuralNet.classify(inputs, getResult);
+  }else {
+    setTimeout(classifyPose, 100);
+  }
+}
+
+function getResult(error, results) {
+  if (results[0].confidence > 0.75) {
+    poseLabel = results[0].label.toUpperCase();
+	workoutCheck(poseLabel);
+  }
+  classifyPose();
+}
+
 function getPoses(poses){
 	if(poses.length > 0){
-		if(poses[0].pose.keypoints[7].score > MIN_PREDICTION_SCORE &&
-		poses[0].pose.keypoints[8].score > MIN_PREDICTION_SCORE){
-			if(workoutCheck(poses)){
-				timer[0] = performance.now();
-				timeSign.innerText = "Warming up!";
-			}else{
-				timeSign.innerText = "Time without warmup: " + getTimerDifference();
-				checkTimerAlert(getTimerDifference());
-			}
-		}else{
-			timeSign.innerText = "Small prediction score!";
-		}
+		pose = poses[0].pose;
 	}
 }
-function workoutCheck(poses){
-	let rightElbY = poses[0].pose.keypoints[8].position.y;
-	let leftElbY = poses[0].pose.keypoints[7].position.y;
-	if(rightElbY < poses[0].pose.keypoints[6].position.y || 
-		leftElbY < poses[0].pose.keypoints[6].position.y){
-			return true;
-	}else{
-		return false;
+
+function workoutCheck(poseLabel){
+	switch(poseLabel){
+		case "G":
+			timeSign.innerText = "Time without warmup: " + getTimerDifference();
+			checkTimerAlert(getTimerDifference());
+			break;
+		case "Q":
+			timer[0] = performance.now();
+			timeSign.innerText = "Warming up!";
+			break;
+		case "W":
+			timer[0] = performance.now();
+			timeSign.innerText = "Warming up!";
+			break;
+		case "E":
+			timer[0] = performance.now();
+			timeSign.innerText = "Warming up!";
+			break;
+		case "R":
+			timer[0] = performance.now();
+			timeSign.innerText = "Warming up!";
+			break;
+		case "T":
+			timer[0] = performance.now();
+			timeSign.innerText = "Warming up!";
+			break;
 	}
+	//console.log(poseLabel);
 }
+
 function getTimerDifference(){
 	timer[1] = performance.now();
 	return parseInt((timer[1] - timer[0])/1000);
 }
+
 function checkTimerAlert(time){
 	if(time > 3600){
 		alert("Please warmup!");
 	}
 }
+
 function draw(poses) {
 	image(capture,0,0);
 }
